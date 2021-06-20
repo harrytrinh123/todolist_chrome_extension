3622c486bafad7e91b34039738748298417c70c0
Jack Trinh <40319325+jacktrinh123@users.noreply.github.com>
2021-06-20 08:33:59 +0700

    double click to update todo

--- popout.js
+++ popout.js
@@ -1,4 +1,4 @@
-
+// Code button event create todo
 document.querySelector('.create-todo').addEventListener('click', function () {
     document.querySelector('.create-todo').style.display = "none";
     document.querySelector('.divThem').style.display = "block";
@@ -6,19 +6,21 @@ document.querySelector('.create-todo').addEventListener('click', function () {
     document.querySelector('#inputTodo').focus();
 });
 
+// Code button event Save todo
 document.querySelector('#btnSave').addEventListener('click', function () {
     var itemName = document.querySelector('#inputTodo').value;
     if (itemName != '') {
         var itemsStorage = localStorage.getItem("todo-items");
         var itemsArr = JSON.parse(itemsStorage);
-        itemsArr.push({ "item": itemName, "status": 0});
+        itemsArr.push({ "item": itemName, "status": 0 });
         saveItems(itemsArr);
         fetchItems();
         document.querySelector('.create-todo').style.display = "block";
         document.querySelector('.divThem').style.display = "none";
     }
-}); 
+});
 
+// Make event enter when create todo
 document.querySelector('#inputTodo').addEventListener('keyup', function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
@@ -26,6 +28,7 @@ document.querySelector('#inputTodo').addEventListener('keyup', function (event) 
     }
 });
 
+// load list items to main
 function fetchItems() {
     const itemList = document.querySelector('ul.todo-items');
     itemList.innerHTML = '';
@@ -48,6 +51,23 @@ function fetchItems() {
 
         var itemsListUL = document.querySelectorAll("ul li");
         for (var i = 0; i < itemsListUL.length; i++) {
+            itemsListUL[i].querySelector('.item').addEventListener('dblclick', function () {
+                var valTodo = this.innerHTML;
+                this.innerHTML = `<input class="inputEdit" type="text" value="${valTodo}">`;
+                this.querySelector('.inputEdit').focus();
+                this.querySelector('.inputEdit').selectionStart = this.querySelector('.inputEdit').selectionEnd = this.querySelector('.inputEdit').value.length;
+                this.querySelector('.inputEdit').addEventListener('keyup', function (e) {
+                    if (e.keyCode === 13) {
+                        var index = this.parentNode.parentNode.dataset.itemindex;
+                        console.log(index);
+                        itemUpdate(index);
+                    }
+                });
+
+            });
+        }
+
+        for (var i = 0; i < itemsListUL.length; i++) {
             itemsListUL[i].querySelector('.itemComplete').addEventListener('click', function () {
                 var index = this.parentNode.parentNode.dataset.itemindex;
                 itemComplete(index);
@@ -56,10 +76,6 @@ function fetchItems() {
                 var index = this.parentNode.parentNode.dataset.itemindex;
                 itemDelete(index);
                 fetchItems();
-                var itemsStorage = localStorage.getItem("todo-items");
-                var Arr = JSON.parse(itemsStorage);
-                console.log(index);
-                console.log(Arr);
             });
         }
     } catch (error) {
@@ -85,7 +101,17 @@ function itemDelete(index) {
     document.querySelector('ul.todo-items li[data-itemindex="' + index + '"]').remove();
 }
 
+function itemUpdate(index) {
+    var itemsStorage = localStorage.getItem("todo-items");
+    var itemsArr = JSON.parse(itemsStorage);
 
+    var itemval = document.querySelector('ul.todo-items li[data-itemindex="' + index + '"] .item input').value;
+    itemsArr[index].item = itemval;
+    saveItems(itemsArr);
+    document.querySelector('ul.todo-items li[data-itemindex="' + index + '"] .item').innerHTML = itemval;
+}
+
+// save items array to localStorage
 function saveItems(obj) {
     var string = JSON.stringify(obj);
     localStorage.setItem('todo-items', string);
